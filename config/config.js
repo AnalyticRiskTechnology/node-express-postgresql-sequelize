e411a4237c71e5f07748a45625a638d846c9bd57
Tim Murnaghan <timmurnaghan@hotmail.com>
2019-08-29 01:10:21 +0100

    fix environments and port

--- config/config.js
+++ config/config.js
@@ -1,24 +1,8 @@
 require('dotenv').config(); // this is important!
 module.exports = {
-  "development": {
-    "username": "djamware",
-    "password": "dj@mw@r3",
-    "database": "node_sequelize",
-    "host": "127.0.0.1",
+    "username": process.env["DATABASE_USER"],
+    "password": process.env["DATABASE_PASSWORD"],
+    "database": process.env["DATABASE_NAME"],
+    "host": process.env["DATABASE_SERVICE_NAME"],
     "dialect": "postgres"
-  },
-  "test": {
-    "username": process.env["POSTGRES_USER"],
-    "password": process.env["POSTGRES_PASSWORD"],
-    "database": process.env["POSTGRES_DATABASE"],
-    "host": process.env["POSTGRESQL_SERVICE_HOST"],
-    "dialect": "postgres"
-  },
-  "production": {
-    "username": "root",
-    "password": "dj@mw@r3",
-    "database": "node_sequelize",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  }
 };
